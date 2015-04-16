85dd3b262786450526ad0fb6ed535b3b9ead1072
Bill-Bo94 <bjoer.hartmann@gmail.com>
2015-04-16 12:45:37 +0200

    Bibliothek hinzugefügt

--- isset.c
+++ isset.c
@@ -3,24 +3,26 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[], char* envp[])
 {
   char* variable = getenv(argv[1]);
   if(variable==NULL)
-  {
-    return 1;
-  }
-  else
-  {
-    if(argc > 2)
-    {
-      if(strcmp(argv[2],"-v")==0)
-      {
-        printf("%s=%s",argv[1],variable);
-        printf("\n");
-      }
-    }
-    return 0;
-  }
+   {
+     printf("NULL\n");
+     return 1;
+   }
+   else
+   {
+     if(argc > 2)
+     {
+       if(strcmp(argv[2],"-v")==0)
+       {
+         printf("%s=%s",argv[1],variable);
+         printf("\n");
+       }
+     }
+     return 0;
+   }
 }
