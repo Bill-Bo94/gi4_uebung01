8e41e3fd679cfb9def8f788cfceb98f5dc773a4e
Bill-Bo94 <bjoer.hartmann@gmail.com>
2015-04-16 12:05:22 +0200

    Uebung 1-3

--- myecho.c
+++ myecho.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+//Dieses Programm gibt alle uebergebenen Parameter (ohne Name des Programms) aus
 
 int main(int argc, char* argv[], char* envp[]) 
 {
+  int k;
+  for(k=1;k<argc;k++)
+  {
+    printf("%s ",argv[k]);
+  }
+  printf("\n");
   return 0;
 }
